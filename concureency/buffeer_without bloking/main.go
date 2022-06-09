5d41a21fe8c9589be321121b957e88cb932b6c14
vinaycharlie01 <106230026+vinaycharlie01@users.noreply.github.com>
2022-06-09 12:49:58 +0530

    Create main.go

---
+++ concureency/buffeer_without bloking/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	ch := make(chan int, 6)
+
+	go func() {
+		defer close(ch)
+		for i := 0; i < 6; i++ {
+			fmt.Printf("Sending %d\n", i)
+			ch <- i
+
+		}
+
+	}()
+	for v := range ch {
+		fmt.Println("recing", v)
+
+	}
+}
