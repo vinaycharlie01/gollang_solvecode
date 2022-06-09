f5af184169c74feac0afea2fd189e9412150dcf9
vinaycharlie01 <106230026+vinaycharlie01@users.noreply.github.com>
2022-06-09 16:11:18 +0530

    Create selectwithdefallt.go

---
+++ concureency/selectwithdefallt.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	ch := make(chan string)
+	go func() {
+		for i := 0; i < 3; i++ {
+			time.Sleep(1 * time.Second)
+			ch <- "Message"
+
+		}
+
+	}()
+
+	for i := 0; i < 2; i++ {
+		select {
+		case m := <-ch:
+			fmt.Println(m)
+		default:
+			fmt.Println("no message")
+
+		}
+
+	}
+
+}
